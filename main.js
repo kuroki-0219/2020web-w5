7d4e8896008eabb98f1e629c01f4534a7907626f
kuroki-0219 <75403490+kuroki-0219@users.noreply.github.com>
2021-01-07 16:15:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText = '気温 華氏40度で外は寒かった。, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
 let insertY = ['the soup kitchen','Disneyland','the White House'];
 let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
@@ -31,7 +31,7 @@ function result() {
     newStory = newStory.replace('Bob',name);
   }
 
-  if(document.getElementById("uk").checked) {
+  if(document.getElementById("日本").checked) {
     const weight = Math.round(300*0.0714286) + ' stone';
     const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
     newStory = newStory.replace('94 fahrenheit',temperature);
